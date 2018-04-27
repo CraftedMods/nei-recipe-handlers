e6d2becfbc9ed90c5cfd340a6cb002b43f3a6081
CraftedMods <craftedMods@t-online.de>
2018-04-27 12:53:55 +0200

    Setup for 2.0.0-alpha

--- src/main/java/craftedMods/recipes/NEIRecipeHandlers.java
+++ src/main/java/craftedMods/recipes/NEIRecipeHandlers.java
@@ -30,7 +30,7 @@ public class NEIRecipeHandlers {
 
 	public static final String MODID = "neirecipehandlers";
 	public static final String MODNAME = "NEI Recipe Handlers";
-	public static final String VERSION = "1.0.0-alpha";
+	public static final String VERSION = "2.0.0-alpha";
 
 	public static final String DEFAULT_VERSION_FILE_URL = "https://dl.dropboxusercontent.com/s/gyz1oq7vyz753y5/version.txt";
 
